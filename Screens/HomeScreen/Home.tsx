d6c0a738abe9c2cc6b6ce738ed98cbc2bbbb45d5
jjarrett21 <james.jarrett@okstate.edu>
2020-05-29 23:37:38 -0500

    adds play button opacity

--- Screens/HomeScreen/Home.tsx
+++ Screens/HomeScreen/Home.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState } from "react";
-import { Text, View, Button } from "react-native";
+import { Text, View, Button, Image, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 import { Header } from "../../Components/Header";
 import PlayArrowIcon from "mdi-react/PlayArrowIcon";
@@ -12,11 +12,18 @@ export const Home: FC = () => {
   };
 
   return (
-    <View style={styles.contaienr}>
+    <View style={styles.container}>
       <Header />
-      <Button title="play" onPress={handePlayPress}>
-        <PlayArrowIcon size={11} color={"#2e856e"} /> PLAY!
-      </Button>
+      <TouchableOpacity
+        onPress={handePlayPress}
+        style={{ alignItems: "center", width: "100%" }}
+      >
+        <Image
+          source={require("../../assets/play-button.png")}
+          style={styles.playIcon}
+        />
+        {/* <Text style={styles.play}>PLAY!</Text> */}
+      </TouchableOpacity>
     </View>
   );
 };
--- Screens/HomeScreen/styles.ts
+++ Screens/HomeScreen/styles.ts
@@ -1,11 +1,21 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  contaienr: {
+  container: {
     flex: 1,
     width: "100%",
     backgroundColor: "#363B5A",
     alignItems: "center",
     justifyContent: "center",
+  },
+  play: {
+    fontSize: 45,
+    color: "#ecf0f1",
+    marginTop: 5,
+  },
+  playIcon: {
+    height: 60,
+    width: 250,
+    paddingTop: 100,
   },
 });
